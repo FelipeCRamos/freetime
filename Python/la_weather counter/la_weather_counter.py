d35420d4383a6f22a4dd19ccab480c2ab7102c7b
FelipeCRamos <felipe.da.costa_10@hotmail.com>
2017-08-03 15:29:37 -0300

    FelipeCRamos

--- Python/la_weather counter/la_weather_counter.py
+++ Python/la_weather counter/la_weather_counter.py
@@ -1,28 +1,35 @@
-# Test
+# Made by FelipeCRamos
 __author__ = "FelipeCRamos"
-print("Please, be welcome to la_weather_counter!")
 
+''' FUNCTIONS '''
 def counter(list, list_element):
     global final_result
     counter = 0
     for element in list_element:
        print(element[:-1],':',list.count(element), 'times ~ %.2f%%' %(list.count(element)/len(list)*100))
 
-file = open('la_weather.csv', 'r')
-lines = []
-weathers = []
-elements_all = []
-for line in file:
-    lines.append(line.split(','))
+def enterFile():
+    filename = input("Please, enter the filename below:\n> ")
+    main(filename)
 
-lines.pop(0)
-# print(lines)
-for i in range(len(lines)):
-    if(lines[i][1] not in weathers):
-        weathers.append(lines[i][1])
-    elements_all.append(lines[i][1])
+def main(filename):
+    try:
+        file = open(filename, 'r')
+        lines, weathers, elements_all = [], [], []
+        for line in file:
+            lines.append(line.split(','))
+        lines.pop(0)
+        for i in range(len(lines)):
+            if(lines[i][1] not in weathers):
+                weathers.append(lines[i][1])
+            elements_all.append(lines[i][1])
+        print('\n' + counter(elements_all, weathers))
+        print("\nExiting...")
 
-# print(weathers)
-print('')
-print(counter(elements_all, weathers))
-print("\nExiting...")
+    except FileNotFoundError:
+        print("\nThe file doesn't exist!\n\nPlease, try again.\n")
+        enterFile()
+
+''' PROGRAM ITSELF '''
+print("Please, be welcome to la_weather_counter!")
+enterFile()
